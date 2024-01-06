91200640fadaca3b6a8599118876fc5070555dd3
Nghia <nghiachicken0511@gmail.com>
2024-01-06 15:45:05 +0700

    update

--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ import { CONNECT_DB, CLOSE_DB } from '~/config/mongodb'
 import { env } from '~/config/environment'
 import { API_V1 } from '~/routes/v1'
 import { errorHandlingMiddleware } from '~/middlewares/errorHandlingMiddleware'
+import { handling404Middleware } from '~/middlewares/handling404Middleware'
 
 const START_SERVER = () => {
   const app = express()
@@ -14,6 +15,8 @@ const START_SERVER = () => {
   app.use(express.json())
 
   app.use('/api/v1', API_V1)
+
+  app.use(handling404Middleware)
 
   app.use(errorHandlingMiddleware)
 
@@ -32,8 +35,6 @@ const START_SERVER = () => {
     await CONNECT_DB()
     START_SERVER()
   } catch (error) {
-    // eslint-disable-next-line no-console
-    console.error(error)
     process.exit(1)
   }
 })()
